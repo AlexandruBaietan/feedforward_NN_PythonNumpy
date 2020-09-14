f20720b7303e701d40adb33bde828c8809813eb7
AlexandruBaietan <alexandru.baietan89@gmail.com>
2020-09-14 21:26:58 +0300

    finishsed first version

--- forwardProp.py
+++ forwardProp.py
@@ -1,17 +1,14 @@
 import numpy as np
 from numpy import random
+from random import random as ran
 
-# Save the activations and derivatives
 
-# implement backpropagation
-
-# Implement gradient descent
 
 # Implement train
 
-# train network with dummy dataset
 
-# make predictions
+
+
 
 
 class Network(object):
@@ -37,9 +34,10 @@ class Network(object):
             weights.append(w)
         self.weights = weights
 
+        # Save the activations and derivatives
         activations = []
         for i in range(len(layers)):
-            a = np.zeros(layers[i])
+            a = np.zeros(layers[i]) 
             activations.append(a)
 
         self.activations = activations
@@ -52,7 +50,7 @@ class Network(object):
         self.derivatives = derivatives
             
 
-        
+   
     def forward_propagate(self, inputs):
         
         activations = inputs
@@ -61,12 +59,70 @@ class Network(object):
         for i, w in enumerate(self.weights):
             #we calculate the new inputs
             net_inputs = np.dot(activations, w)
-            print (f"activations = {activations},\n weights = {w},\n result = {net_inputs}")
+            #print (f"activations = {activations},\n weights = {w},\n result = {net_inputs}")
             #calculate the activations
             activations = self.sigmoid(net_inputs)
             self.activations[i+1] = activations
             
         return activations
+
+     # implement backpropagation  
+    def backpropagate(self, error, verbose=False):
+
+        # dE/dW[i] = (y  - a[i+1]) * s'(h[i+1]) * a[i]
+        # s'(h[i+1]) s(h[i+1]) * (1 - s(h[i+1]))
+        # s(h[i+1]) = a[i+1]
+        #dE/dW[i - 1] = (y - a[i+1] * s'[i+1]) * s'(h[i+1]) W[i] * s'(h[i]]) * a[i-1]
+        for i in reversed(range(len(self.derivatives))):
+            activations = self.activations[i+1]
+            delta = error * self.sigmoid_derivative(activations) # --> ndarry([0.1, 0.2]) --> ndarray([[0.1, 0.2]])
+            delta_reshaped = delta.reshape(delta.shape[0], -1).T
+            current_activations = self.activations[i] # --> ndarry([0.1, 0.2]) --> ndarray([[0.1], [0.2]])
+            current_activations_reshaped = current_activations.reshape(current_activations.shape[0], -1)
+            self.derivatives[i] = np.dot(current_activations_reshaped, delta_reshaped)
+            error = np.dot(delta, self.weights[i].T)
+
+            if(verbose):
+                print(f"Derivatives for W{i}: {self.derivatives[i]}")
+        return error
+
+    # Implement gradient descent
+
+    def gradient_descent(self, learning_rate):
+        
+        for i in range(len(self.weights)):
+            weights = self.weights[i]
+            #print(f"Original W{i},  {weights}")
+            derivatives = self.derivatives[i]
+            weights += derivatives * learning_rate 
+            #print(f"updated W{i},  {weights}")
+
+
+# train network with dummy dataset
+    def train(self, inputs, targets, epochs, learning_rate):
+        
+        for i in range(epochs):
+            sum_error = 0
+            for j, (input, target) in enumerate(zip(inputs, targets)):
+                output = self.forward_propagate(input)
+                #calculate_error
+                error = target - output
+                #backpropagation
+                self.backpropagate(error)
+                #apply gradient descent
+                self.gradient_descent(learning_rate)
+                #report error
+                sum_error += self.mse(target, output)
+            print(f"Error: {sum_error/len(inputs)} at epoch {i}")
+
+
+    def mse(self, target, output):
+        return np.average((target - output) ** 2)
+
+
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
 
     def sigmoid(self, x):
         return 1 /(1 + np.exp(-x))
@@ -74,12 +130,22 @@ class Network(object):
 
 if __name__ == '__main__':
    
-   # create a MLP
-    nn = Network()
-   # create some inputs
-    inputs = random.rand(nn.num_inputs)
-   # forward prop
-    outputs = nn.forward_propagate(inputs)
-   # print result
-    print(f"Network input is: {inputs}")
-    print(f"Network output is: {outputs}")
+    # create a MLP
+    nn = Network(2, [5], 1)
+   
+    # create some inputs
+    inputs = np.array([[ran() / 2 for _ in range(2)] for _ in range(1000)])
+    target = np.array([[i[0] + i[1]] for i in inputs])
+     # train model
+    nn.train(inputs, target, 100, 0.3)
+    
+    # make predictions
+
+    #create dummy data
+
+    input = np.array([.7, .1])
+    target = np.array([.8])
+
+    output = nn.forward_propagate(input)
+    print(f"Our network believes that, {input[0]} + {input[1]} equals {output}")
+   
