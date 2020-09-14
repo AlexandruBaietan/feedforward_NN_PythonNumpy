4d2b62d0b4ed1c7ae77f5800103e75140b16be26
AlexandruBaietan <alexandru.baietan89@gmail.com>
2020-09-14 11:29:05 +0300

    performed forward prop without bias

--- forwardProp.py
+++ forwardProp.py
@@ -1,7 +1,8 @@
 import numpy as np
+from numpy import random
 
 
-class MLP:
+class Network:
 
     def __init__(self, num_inputs=3, num_hidden=[3, 5], num_outputs=2):
 
@@ -17,9 +18,40 @@ class MLP:
         #initiate random weights
         self.weights = []
         for i in range(len(layers)-1):
-            #we create the random matrix
-            w = np.random.rand(layers[i], layers[i+1])
+            #we create the random matrix of weights to connect to each layer
+            #layer[i] is connected to layer[i+1]
+            #So we get a layer[i] x layer[i+1] matrix 
+            w = random.rand(layers[i], layers[i+1])
+            self.weights.append(w)
+        for w in self.weights:
             
+            #we calculate the new inputs
+            print (f"these are the individual weight matrix {w}")
+    def forward_propagate(self, inputs):
+        
+        activations = inputs
+        #we are looping through all the weight matrices (layers)
+        for w in self.weights:
+            #we calculate the new inputs
+            
+            net_inputs = np.dot(activations, w)
+            print (f"activations = {activations},\n weights = {w},\n result = {net_inputs}")
+            #calculate the activations
+            activations = self.sigmoid(net_inputs)
+            
+        return activations
+    def sigmoid(self, x):
+        return 1 /(1 + np.exp(-x))
+
 
 if __name__ == '__main__':
    
+   # create a MLP
+    nn = Network()
+   # create some inputs
+    inputs = random.rand(nn.num_inputs)
+   # forward prop
+    outputs = nn.forward_propagate(inputs)
+   # print result
+    print(f"Network input is: {inputs}")
+    print(f"Network output is: {outputs}")
